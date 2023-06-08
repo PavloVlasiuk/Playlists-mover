c1dd3cd2c2adb15b339019491650c05ecbef84cd
PavloVlasiuk <pavlo.vlasuk@gmail.com>
2023-06-09 01:58:12 +0300

    added new functions to work with SpotifyAPI

--- server/spotifyController.js
+++ server/spotifyController.js
@@ -1,6 +1,7 @@
 "use strict";
 
 export const spotifyController = (() => {
+  //returns an ID of current user
   const _getUserId = async (token) => {
     const request = await fetch("https://api.spotify.com/v1/me", {
       method: "GET",
@@ -10,10 +11,10 @@ export const spotifyController = (() => {
     });
 
     const userData = await request.json();
-
     return userData.id;
   };
 
+  //creates playlist and returns it's id
   const _createPlaylist = async (token, userId, playlistName) => {
     const requstBody = {
       name: playlistName,
@@ -33,14 +34,61 @@ export const spotifyController = (() => {
     );
 
     const playlistData = await request.json();
-
     return playlistData.id;
+  };
+
+  //searches songs by name and artist and returns an array of uris
+  const _searchTracks = async (token, tracks) => {
+    const headers = {
+      method: "GET",
+      headers: {
+        Authorization: "Bearer " + token,
+      },
+    };
+
+    const requests = tracks
+      .map((item) => encodeURIComponent(`${item.track} artist:${item.artist}`))
+      .map((item) =>
+        fetch(
+          `https://api.spotify.com/v1/search?q=${item}&type=track&limit=1&offset=0`,
+          headers
+        )
+      );
+
+    return Promise.all(requests)
+      .then((responses) => Promise.all(responses.map((r) => r.json())))
+      .then((items) => items.map((item) => item.tracks.items[0].uri));
+  };
+
+  //adds tracks to created playlist
+  const _addTracksToPlaylist = async (token, playlistId, trackUris) => {
+    const requestBody = {
+      uris: trackUris,
+    };
+
+    const request = await fetch(
+      `https://api.spotify.com/v1/playlists/${playlistId}/tracks`,
+      {
+        method: "POST",
+        headers: {
+          Authorization: "Bearer " + token,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(requestBody),
+      }
+    );
+
+    const response = await request.json();
+    return response;
   };
 
   return {
     getUserId: _getUserId,
 
     createPlaylist: _createPlaylist,
+
+    searchTracks: _searchTracks,
+
+    addTracksToPlaylist: _addTracksToPlaylist,
   };
-  
 })();
