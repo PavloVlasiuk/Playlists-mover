0c89ebc10015da73bdbd323aca1a99c5fb097b6f
PavloVlasiuk <pavlo.vlasuk@gmail.com>
2023-06-10 17:59:17 +0300

    added client credentials

---
+++ client/configs/credentials.js
@@ -0,0 +1,7 @@
+export const YT_CLIENT_ID = "583365670984-cbqtrcpn3enooutceqf0408klspbmcfq.apps.googleusercontent.com";
+export const YT_CLIENT_SECRET = "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";
+export const YT_AUTHORIZE = "https://accounts.google.com/o/oauth2/v2/auth?";
+export const YT_SCOPES = ["https://www.googleapis.com/auth/youtube.readonly"]
+
+export const SPOTIFY_CLIENT_ID = "69d9210db06a44d582b7217182ad5a09";
+export const SPOTIFY_CLIENT_SECRET = "d99d9ed5c3bd48198bcfcfb387a16d65";
