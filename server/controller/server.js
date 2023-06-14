6597877b8cc171d062ca7478046ff0dd1c82bd2e
PavloVlasiuk <pavlo.vlasuk@gmail.com>
2023-06-14 13:07:47 +0300

    allowed any cors

--- server/controller/server.js
+++ server/controller/server.js
@@ -4,7 +4,17 @@ import http from "http";
 import { PORT, HOSTNAME } from "../configs/serverConfigs.js";
 import { router } from "./storageController.js";
 
-const server = http.createServer(router);
+const server = http.createServer((req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*")
+  res.setHeader("Access-Control-Allow-Headers", "*");
+  res.setHeader("Access-Control-Allow-Methods", "DELETE, POST, GET");
+  if (req.method === 'OPTIONS') {
+    res.writeHead(200);
+    res.end();
+    return;
+  }
+  router(req, res);
+});
 
 server.listen(PORT, HOSTNAME, () => {
   console.log(`Listening to http://${HOSTNAME}:${PORT}`);
