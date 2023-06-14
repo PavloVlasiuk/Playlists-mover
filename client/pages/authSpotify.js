018d17b3bfd76053f1df30c8a74df321a3bbeb01
chErnykovaDD <chernykova.d@gmail.com>
2023-06-15 01:28:38 +0300

    deleted unnecessary files

--- client/pages/authSpotify.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-export const toAuthSpotify = () => {
-  document.querySelector(".content-flow").innerHTML = `
-    <div class="container">
-      <button id="auth-spotify">Click</button>
-    </div>
-  `;
-};
